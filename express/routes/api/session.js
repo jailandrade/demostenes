1ab8a93cea0eecc0e259ea47064bdf5895e4fc7a
Mikhail Cruz Andrade <jailandrade@gmail.com>
2018-03-13 22:47:08 -0600

    separate the routes for web and for an api standalone version

---
+++ express/routes/api/session.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET session listing. */
+router.get('/login', function(req, res, next) {
+  
+});
+
+router.get('/register', function(req, res, next) {
+  
+});
+
+
+module.exports = router;
--- express/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET dashboard listing. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard/index', { title: 'Demostenes' });
-});
-
-module.exports = router;
